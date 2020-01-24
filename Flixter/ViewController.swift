25035267f419a569690c04d74fcc7264f7d37939
Jose Patino <jose.patino88@icloud.com>
2020-01-24 12:01:14 -0500

    Initial Commit

---
+++ Flixter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixter
+//
+//  Created by Jose Patino on 1/24/20.
+//  Copyright © 2020 Jose Patino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
