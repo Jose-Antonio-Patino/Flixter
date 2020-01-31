c709a36f278453b77d33af479a0b2cae60e84c64
Jose Patino <jose.patino88@icloud.com>
2020-01-31 12:44:47 -0500

    Superhero Tab

---
+++ Flixter/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flixter
+//
+//  Created by Jose Patino on 1/31/20.
+//  Copyright © 2020 Jose Patino. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
